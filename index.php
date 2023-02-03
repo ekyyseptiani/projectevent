1588510857c33e0444abb808d095951b155cd54d
ekyyseptiani <124320605+ekyyseptiani@users.noreply.github.com>
2023-02-03 11:15:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1><center>INFORMASI</center></h1>
+	<P><h3><center>Festival musik menjadi event yang paling ditunggu oleh banyak masyarakat Indonesia, khususnya para kaum muda.<br>
+
+Terlebih, usai berakhirnya pandemi Covid-19 yang membuat festival musik terhenti penyelenggaraannya selama dua tahun.<br>
+
+Berbagai festival musik telah kembali digelar sejak awal tahun 2022 ini yang tentunya disambut gembira oleh kaum muda Indonesia.<br>
+
+Di sisa tahun 2022 ini masih banyak terdapat festival musik yang akan diselenggarakan di berbagai tempat.</center></h3></P>
+<p><center><h2>Jika Anda Berminat Mengikuti Maka Klik Link dibawah Ini</h2></center></p>
+
+<center><nav><a href="beranda.php">LOGIN
+</a></nav></center>
+
+</body>
+</html>
